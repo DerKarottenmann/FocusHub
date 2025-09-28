1c3f9f9e58d8d55107a3abf15c996d1a34d45ab2
DerKarottenmann <yannikdangel@icloud.com>
2025-09-28 12:39:34 +0200

    28Sep2025

---
+++ Software/MC_Code/Main-Code _WemosD1mini/include/keys_example.h
@@ -0,0 +1,7 @@
+// When using the code, please replace "keys_example.h" with your own "keys.h" file containing your actual keys.
+// secrets.example.h
+#pragma once
+const char* API_KEY = "Google_API_Key_here";
+const char* POSTAL_CODE = "Your_Postal_Code_here";
+const char* COUNTRY_CODE = "Your_Country_Code_here";
+
